a81c04a81e6d8793485988f76ac75e96f486b4f0
axiu <abawor200@gmail.com>
2023-11-17 11:13:03 +0000

    App now displays added items

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://mobileappshoppingbasket-default-rtdb.europe-west1.firebasedatabase.app/"
+
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const cartItemsInDB = ref(database, "cartitems");
+
 const inputEl = document.getElementById("input-field");
 const addBtn = document.getElementById("add-button");
-let cartItems = [];
-
+const shoppingList = document.getElementById("shopping-list");
+let cartItems = ["milk", "banana"];
 
 addBtn.addEventListener("click", function() {
     let inputValue = inputEl.value
-    cartItems.push(inputValue)
-    inputEl.value = ""
-    console.log(cartItems)
+    if (inputValue) {
+        cartItems.push(inputValue)
+        push(cartItemsInDB, inputValue)
+        inputEl.value = ""
+    }
+    renderItems()
+});
+
+function renderItems() {
+    let showItems = ""
+    for (let i = 0; i < cartItems.length; i++) {
+        showItems += `
+            <li>
+                <button>${cartItems[i]}</button>
+            </li>
+        `
+    }
+    shoppingList.innerHTML = showItems
+};
+
+window.addEventListener("load", function() {
+    renderItems()
 });
